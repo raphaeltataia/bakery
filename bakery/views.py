01d298df0bdc40655050363d1a12595886cbba9a
Raphael Tataia <raphaeltataia@Raphaels-MacBook-Pro.local>
2017-04-26 13:03:58 -0300

    Modified templates to display posts from database

--- bakery/views.py
+++ bakery/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 from django.http import HttpResponse
 
 # developed by @raphaeltataia
 
+from .models import Recipe
+
 def post_list(request):
-    return render(request, 'bakery/post_list.html', {})
+    recipes = Recipe.objects.order_by('created_date')
+    return render(request, 'bakery/post_list.html', {'recipes': recipes})
